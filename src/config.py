a4c98e8b31e8cbf82b1e30afffb45abc6a153dda
Kpiiale <crhystelvelasco213@gmail.com>
2025-06-09 18:19:07 -0500

    Start of url shortener microservice

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings):
+    #lee las variables de entorno
+    databaseUrl: str="sqlite:///./shortener.db"
+    baseUrl:str="http://localhost:8000"
+    
+    #configuracion para pydantic-settings
+    model_config=SettingsConfigDict(env_file=".env")
+    
+#se crea una unica instancia
+settings=Settings()
